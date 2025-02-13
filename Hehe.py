533cadf6fbb7a8c12c0d7475c85752db571e4adb
Irham walzoo <106699783+irhamwalzoo@users.noreply.github.com>
2025-02-13 18:24:53 +0700

    Update Hehe.py

--- Hehe.py
+++ Hehe.py
@@ -1,7 +1,14 @@
-nama = "IRHAM"
+nama = "🀄IRHAM🀄"
 garis = "-" * (len(nama) + 4)
 print(f"{garis}\n| {nama} |\n{garis}")
 
+import time
 while True: 
   print("awal dari kesederhanaan akan menciptakan kebesaran di masa depan")
-  time.sleep(3)
+  time.sleep(0.5)
+while True: 
+  print("ʕ⁠っ⁠•⁠ᴥ⁠•⁠ʔ⁠っ")
+  time.sleep(0.6) 
+while True: 
+  print("TUKANG WIFI TUKANG INSTALL WINDOWS")
+  time.sleep(1)
