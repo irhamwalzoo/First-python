9ec9c974a9b77136b032a510fdf4781f40e27217
Irham walzoo <106699783+irhamwalzoo@users.noreply.github.com>
2025-02-13 18:29:34 +0700

    Update Hehe.py

--- Hehe.py
+++ Hehe.py
@@ -6,9 +6,11 @@ import time
 while True: 
   print("awal dari kesederhanaan akan menciptakan kebesaran di masa depan")
   time.sleep(0.5)
+  
 while True: 
   print("ʕ⁠っ⁠•⁠ᴥ⁠•⁠ʔ⁠っ")
   time.sleep(0.6) 
+  
 while True: 
   print("TUKANG WIFI TUKANG INSTALL WINDOWS")
   time.sleep(1)
