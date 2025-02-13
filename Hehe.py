dae87e42d0302e60c9c2fbde44a0f4953aceb092
Irham walzoo <106699783+irhamwalzoo@users.noreply.github.com>
2025-02-13 18:12:34 +0700

    Create Hehe.py

---
+++ Hehe.py
@@ -0,0 +1,7 @@
+nama = "IRHAM"
+garis = "-" * (len(nama) + 4)
+print(f"{garis}\n| {nama} |\n{garis}")
+
+while True: 
+  print("awal dari kesederhanaan akan menciptakan kebesaran di masa depan")
+  time.sleep(3)
